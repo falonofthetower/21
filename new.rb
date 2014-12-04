dc5e092361187404d564c299ea490875248c5783
Peter Karth <falonofthetower@gmail.com>
2014-12-04 15:31:20 -0600

    Functional BlackJack

--- new.rb
+++ new.rb
@@ -1,20 +1,28 @@
 require 'pry'
 
-suits = ['H', 'D', 'S', 'C']
-face = ['2','3','4','5','6','7','8','9','10','J','Q','K','A']
-deck = suits.product(face)
-deck.shuffle!
+# Helper methods
+
+def player_broke?(player)
+  player[:chips] < 0
+end
+
+def create_deck
+  suits = ['H', 'D', 'S', 'C']
+  face = ['2','3','4','5','6','7','8','9','10','J','Q','K','A']
+  deck = suits.product(face)
+  deck.shuffle!
+end
 
 def calculate_total(hand)
   total = 0
   aces = 0
   count = hand.map { |each| each[1] }
   count.each do |value|
-    if value.to_i == 0
-      total += 10
-    elsif value == 'A'
+    if value == 'A'
       total += 11
       aces += 1
+    elsif value.to_i == 0
+      total += 10    
     else
       total += value.to_i      
     end
@@ -28,81 +36,238 @@ def calculate_total(hand)
 end
 
 def bust?(player)
-  calculate_total(player[:hand]) > 21 ? true : false
+  calculate_total(player[:hand]) > 21
 end
 
-def blackjack(player)
-  calculate_total(player[:hand]) == 21 ? player[:blackjack] = 1 : false  
+def blackjack?(player)
+   calculate_total(player[:hand]) == 21 && player[:hand].count == 2
 end
 
-player = {}
-dealer = {}
-puts "Welcome to BlackJack"
+def check_for_blackjacks?(player,dealer)
+  blackjack?(player) ? player[:blackjack] = true : false
+  blackjack?(dealer) ? dealer[:blackjack] = true : false
+  if (player[:blackjack] == true) || (dealer[:blackjack] == true)
+    true
+  else
+    false
+  end
+end
 
-puts "You must give your name to play here:"
-player[:name] = gets.chomp
+def deck_low?(deck)
+  deck.count < 20  
+end
 
-puts "How many chips do you want?"
-player[:chips] = gets.chomp.to_i
-begin
-  puts player[:chips]
+def deal_card(player,deck)
+  deck.shuffle! if deck_low?(deck)
+  player[:hand] << deck.pop  
+end
+
+# Display methods
+
+def display_hand(player)  
+  top_line = ""
+  denomination_line = ""
+  suit_line = ""
+  side_line = ""
+  bottom_line = ""
+  player[:hand].each do |card|
+     top_line <<             " ---  "
+     if card[1] == '10' 
+      denomination_line <<    "| #{card[1]}| "
+    else
+      denomination_line <<    "| #{card[1]} | "
+    end
+     suit_line <<            "| #{card[0]} | "
+     side_line <<            "|   | "
+     bottom_line <<          " ---  "
+  end
+  puts top_line
+  puts denomination_line
+  puts suit_line
+  puts side_line
+  puts bottom_line
+end
+
+def clear_screen
+  print "\e[2J\e[f"
+end 
+
+def display_table(player,dealer)
+  edge = '***********************************************************************'  
+  system("clear")    
+  puts edge
+  display_hand(dealer)
+  puts ""
+  puts ""
+  puts ""
+  puts ""
+  display_hand(player)
+  puts edge
+  sleep 0.5
+end
+
+def ask_for_chips
+  begin
+    puts "How many chips do you want?"
+    chips = gets.chomp.to_i
+    if chips == 0
+      puts "You must have chips to enter!"
+    else
+      valid = true
+    end
+  end until valid
+  chips
+end
+
+def ask_for_name
+  begin
+    puts "Your name?"
+    name = gets.chomp.to_s
+    if name == ""
+      puts "No Name NO ENTRY!"
+    else
+      valid = true
+    end
+  end until valid
+  name
+end
+
+# Play flow functions
+
+def introduction(player)  
+  
+  puts "Welcome to BlackJack"
+
+  player[:name] = ask_for_name
+
+  player[:chips] = ask_for_chips
+end
+
+def start_round(player,dealer,deck)  
+  player[:bet] = 0
   player[:hand] = []
-  dealer[:hand] = []
-  player[:blackjack] = []
-  dealer[:blackjack] = []
+  dealer[:hand] = []  
+  player[:bust] = false
+  dealer[:bust] = false
+  player[:blackjack] = false
+  dealer[:blackjack] = false
 
-  player[:hand] << deck.pop
-  dealer[:hand] << deck.pop 
-  player[:hand] << deck.pop
-  dealer[:hand] << deck.pop
 
-  blackjack(player) 
-  blackjack(dealer)
-
+  puts "#{player[:name]} Chips: #{player[:chips]}"
   puts "Place your bet!"  
-  bet = gets.chomp.to_i
-  player[:chips] -= bet.to_i
-  begin    
-    my_total = calculate_total(player[:hand])
+
+  player[:bet] = gets.chomp.to_i
+  player[:chips] -= player[:bet]
+  
+  
+  
+  display_table(player,dealer) 
+  deal_card(player,deck)
+  display_table(player,dealer)  
+  deal_card(dealer,deck)
+  display_table(player,dealer)  
+  deal_card(player,deck)
+  display_table(player,dealer)  
+  deal_card(dealer,deck)
+  display_table(player,dealer)
+end
+
+def play_player(player,dealer,deck)
+  begin
+    blackjack = check_for_blackjacks?(player,dealer)
+
+    player_total = calculate_total(player[:hand])
     dealer_total = calculate_total(dealer[:hand])
-    puts "Your total is: #{my_total}, dealer has: #{dealer_total}"
+    #display_table(player,dealer)
+    puts "Your total is: #{player_total}, dealer has: #{dealer_total}"
     puts "Hit or Stay? h/s"
     hit_or_stay = gets.chomp
     if hit_or_stay == 'h'    
-      player[:hand] << deck.pop
+      deal_card(player,deck)
+    display_table(player,dealer)
     elsif hit_or_stay != 's'
       puts "pick (h) or (s)"    
     end
-    my_total = calculate_total(player[:hand])
-    bust = bust?(player)  
-  end until hit_or_stay == 's' || bust
+    player_total = calculate_total(player[:hand])
+    player[:bust] = bust?(player)  
+  end until (hit_or_stay == 's') || (player[:bust] == true) || blackjack
+end
 
-  unless bust  
-    while dealer_total < 17
+def play_dealer(player,dealer,deck)
+  unless player[:bust] || player[:blackjack]
+    dealer_total = calculate_total(dealer[:hand])
+    while dealer_total < 17      
+      deal_card(dealer,deck)
+      display_table(player,dealer)      
+      dealer[:bust] = bust?(dealer)    
       dealer_total = calculate_total(dealer[:hand])
-      dealer[:hand] << deck.pop      
-      bust = bust?(dealer)    
     end
   end
+end
 
-  puts "My total: #{my_total}"
-  puts "Dealer total: #{dealer_total}"  
-  #binding.pry 
-  if my_total > 21
+def determine_winner(player,dealer)
+  dealer_total = calculate_total(dealer[:hand])
+  player_total = calculate_total(player[:hand])
+  puts "My total: #{player_total}"
+  puts "Dealer total: #{dealer_total}"    
+  if player_total > 21
     puts "#{player[:name]} busts!"
-  elsif player[:blackjack] == 1
-    player[:chips] += bet * 2.5
+  elsif (blackjack?(player) && blackjack?(dealer)) || player_total == dealer_total
+    player[:chips] += player[:bet]
+    puts "Push"
+  elsif blackjack?(dealer)
+    puts "Dealer BlackJack!"
+  elsif player_total > 21
+    puts "#{player[:name]} busts!"
+  elsif blackjack?(player)
+    player[:chips] += player[:bet] * 2.5
     puts "BlackJack!"    
-  elsif my_total > dealer_total
-    player[:chips] += bet * 2
+  elsif player_total > dealer_total
+    player[:chips] += player[:bet] * 2
     puts "#{player[:name]} Wins!"
   elsif dealer_total > 21
-    player[:chips] += bet * 2
+    player[:chips] += player[:bet] * 2
     puts "Dealer Busts"      
-  elsif dealer_total > my_total
-    puts "Dealer wins!"
-  elsif my_total == dealer_total
-    player[:chips] += bet
-    puts "Push"  
+  elsif dealer_total > player_total
+    puts "Dealer wins!"  
   end
+end
+
+def test_for_broke(player)
+  if player_broke?(player)
+    system("clear")
+    sleep 3
+    puts "You awake in the alley"
+    sleep 2
+    puts "You've been beaten badly"    
+    sleep 2
+    puts "There is a tattoo on your forehead: #{player[:name]} Owes $#{player[:chips] * -1}"
+    sleep 2
+    abort
+  end
+end  
+
+# Initiate
+
+player = {}
+dealer = {}
+
+deck = create_deck
+
+introduction(player)
+
+# Play Loop
+
+begin
+
+  start_round(player,dealer,deck)
+
+  play_player(player,dealer,deck)
+
+  play_dealer(player,dealer,deck)
+  
+  determine_winner(player,dealer)
+
+  test_for_broke(player)
+
 end until false
---
+++ test.rb
@@ -0,0 +1,4 @@
+require 'pry'
+set_of_cards = [{ suit: "hearts", color: "red", face_value: "A", value: 11 },
+                { suit: "hearts", color: "red", face_value: "2", value: 2 }]                
+binding.pry
