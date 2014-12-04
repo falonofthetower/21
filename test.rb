ae516f6db6a64e841dad89ea556f6d06fd025bf1
Peter Karth <falonofthetower@gmail.com>
2014-12-04 17:52:16 -0600

    removed old version

--- test.rb
+++
@@ -1,4 +0,0 @@
-require 'pry'
-set_of_cards = [{ suit: "hearts", color: "red", face_value: "A", value: 11 },
-                { suit: "hearts", color: "red", face_value: "2", value: 2 }]                
-binding.pry
